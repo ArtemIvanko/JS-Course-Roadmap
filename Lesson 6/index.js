2d3e663cc750148800fbe58fd782023518ff27d1
ArtemIvanko <tema30k@gmail.com>
2024-02-05 21:19:19 +0200

    Added filter method

--- Lesson 6/index.js
+++ Lesson 6/index.js
@@ -1,10 +1,20 @@
 const arr = [false, 12, '23424', false, 324, '', true, null, 5, -0, 0, 9, undefined, NaN, 6.354, undefined, 7, null];
-const newArr = [];
+const newArr1 = [];
+const newArr2 = [];
 
 for (let i = 0; i < arr.length; i++) {
     if (arr[i]) {
-        newArr.push(arr[i]);
+        newArr1.push(arr[i]);
     }
 }
 
-console.log(newArr);
+console.log(newArr1);
+
+// And using filter method
+arr.filter((item) => {
+    if (item) {
+        newArr2.push(item)
+    }
+})
+
+console.log(newArr2)
