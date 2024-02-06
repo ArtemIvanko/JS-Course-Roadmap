e02434e35c2f4f8a9408b9d6685e5823fd72848f
ArtemIvanko <tema30k@gmail.com>
2024-02-06 13:00:33 +0200

    Updated filter functionality

--- Lesson 6/index.js
+++ Lesson 6/index.js
@@ -1,7 +1,9 @@
 const arr = [false, 12, '23424', false, 324, '', true, null, 5, -0, 0, 9, undefined, NaN, 6.354, undefined, 7, null];
 const newArr1 = [];
-const newArr2 = [];
+const newArr2 = []; //filter method 1
 const newArr3 = [];
+const newArr4 = arr.filter((item) => !!item); //filter method 2
+const newArr5 = arr.filter((item) => Boolean(item)); //filter method 3
 
 for (let i = 0; i < arr.length; i++) {
     if (arr[i]) {
@@ -11,14 +13,14 @@ for (let i = 0; i < arr.length; i++) {
 
 console.log(newArr1);
 
-// And using filter method
+// And using filter method. Method 1:
 arr.filter((item) => {
-    if (item) {
+    if (item !== 0 && item !== null && item !== false && item !== undefined && item !== null && item !== "" && !isNaN(item)) {
         newArr2.push(item)
     }
 })
 
-console.log(newArr2)
+console.log(newArr2);
 
 // And using forEach method
 
@@ -28,6 +30,8 @@ arr.forEach((item) => {
     }
 })
 
-console.log(newArr3)
+console.log(newArr3);
+console.log(newArr4);
+console.log(newArr5);
 
 //PS: За код стайл или нейминг особо не обращай внимания, это просто примеры разных методов и оно как-то так получилось...
