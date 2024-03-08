16d9d45e850634558b17ca8d482b833dac624789
ArtemIvanko <tema30k@gmail.com>
2024-03-08 12:03:50 +0200

    Project init, simple list created

---
+++ Lesson 13-16/index.js
@@ -0,0 +1,29 @@
+const arr = [];
+const maxArrLength = +prompt("Enter the number of points");
+
+const mapArrList = (maxValue) => {
+    if (maxValue > 10 || maxValue <= 0) {
+        return alert("The number of points should be between 0 and 10")
+    }
+
+    const pointValue = prompt("Enter a point") || "Empty Point";
+
+    for (let i = 0; i < maxValue; i++) {
+        arr.push(pointValue)
+    }
+}
+
+mapArrList(maxArrLength);
+
+const createList = (arr) => {
+    const list = document.createElement("ul");
+    document.body.appendChild(list);
+
+    arr.forEach((item) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = item;
+        list.appendChild(listItem);
+    })
+}
+
+createList(arr);
