cfadd6bbd966bf41876aec872e2d1df8c035f8ba
ArtemIvanko <tema30k@gmail.com>
2024-03-16 12:01:27 +0200

    Run prettier

--- Lesson 13-16/index.js
+++ Lesson 13-16/index.js
@@ -1,72 +1,72 @@
 const arr = [];
 
 const setDataStorage = () => {
-    localStorage.setItem("points", JSON.stringify(arr));
+  localStorage.setItem("points", JSON.stringify(arr));
 }
 
 const getDataStorage = (arr) => {
-    const getDataItem = JSON.parse(localStorage.getItem("points"));
+  const getDataItem = JSON.parse(localStorage.getItem("points"));
 
-    if (getDataItem !== null) {
-        getDataItem.forEach((item) => {
-            arr.push(item);
-        });
-    }
+  if (getDataItem !== null) {
+    getDataItem.forEach((item) => {
+      arr.push(item);
+    });
+  }
 }
 
 const getPoint = () => {
-    getDataStorage(arr)
+  getDataStorage(arr)
 
-    while (true) {
-        const pointValue = prompt('Enter a point (or click "Cancel" to finish):');
-        if (pointValue === null) break;
-        arr.push(pointValue);
-    }
+  while (true) {
+    const pointValue = prompt('Enter a point (or click "Cancel" to finish):');
+    if (pointValue === null) break;
+    arr.push(pointValue);
+  }
 };
 
 //TODO: rename "customButton"
 const createButton = (name, parent, callback) => {
-    const button = document.createElement("button");
-    button.textContent = name;
-    parent.appendChild(button);
-    button.addEventListener("click", callback);
+  const button = document.createElement("button");
+  button.textContent = name;
+  parent.appendChild(button);
+  button.addEventListener("click", callback);
 }
 
 const createPoint = (item, list) => {
-    setDataStorage();
+  setDataStorage();
 
-    const listItem = document.createElement("li");
-    listItem.textContent = item + " ";
-    list.prepend(listItem);
+  const listItem = document.createElement("li");
+  listItem.textContent = item + " ";
+  list.prepend(listItem);
 
-    createButton("Remove", listItem, () => {
-        arr.splice(arr.indexOf(item), 1);
-        localStorage.setItem("points", JSON.stringify(arr));
-        list.removeChild(listItem);
-    });
+  createButton("Remove", listItem, () => {
+    arr.splice(arr.indexOf(item), 1);
+    localStorage.setItem("points", JSON.stringify(arr));
+    list.removeChild(listItem);
+  });
 };
 
 const createList = (arr) => {
-    const list = document.createElement("ul");
-    document.body.appendChild(list);
-    arr.forEach((item) => {
-        createPoint(item, list);
-    });
+  const list = document.createElement("ul");
+  document.body.appendChild(list);
+  arr.forEach((item) => {
+    createPoint(item, list);
+  });
 };
 
 createButton("Add point", document.body, () => {
-    const pointValue = prompt('Enter a point:');
-    if (pointValue !== null) {
-        arr.push(pointValue);
-        createPoint(pointValue, document.querySelector("ul"));
-    }
+  const pointValue = prompt('Enter a point:');
+  if (pointValue !== null) {
+    arr.push(pointValue);
+    createPoint(pointValue, document.querySelector("ul"));
+  }
 });
 
 createButton("Clear", document.body, () => {
-    localStorage.clear();
-    arr.length = 0;
-    document.querySelector("ul").remove();
-    createList(arr);
+  localStorage.clear();
+  arr.length = 0;
+  document.querySelector("ul").remove();
+  createList(arr);
 });
 
 getPoint();
